d205f0ef555a9e1705630e7c3b80b043b2ad8061
PanzerWeb <selwynvillar@gmail.com>
2026-06-02 20:21:18 +0800

    Added a small indicator that API is connected and a Link is provided

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,6 +3,7 @@ export let API_URL_DEV:string = import.meta.env.VITE_API_URL;
 export function isConnected():boolean{
     if (API_URL_DEV == undefined) {
         console.log("API URL is not connected")
+        console.log(`API URL Link not connected: ${API_URL_DEV}`);
         return false;
     }
 
