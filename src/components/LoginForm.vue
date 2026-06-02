f09174427940149b219572d2fcb587a96ecc43d5
PanzerWeb <selwynvillar@gmail.com>
2026-06-02 20:17:31 +0800

    Added show and hide password

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,61 +1,76 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { labelTailWindClass, inputTailWindClass } from "@/styles/GlobalStyles";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/vue/16/solid";
 
-import { ref } from 'vue';
-import { labelTailWindClass, inputTailWindClass } from '@/styles/GlobalStyles';
-
-
-const username_value = ref('');
-const password_value = ref('');
+const username_value = ref("");
+const password_value = ref("");
+const showPassword = ref(false);
 
 const emit = defineEmits<{
-    (
-        // First parameter, very important from submit-login, it will be identified in the parent component as onSubmitLogin
-        event: 'submit-login', 
-        // Second parameter, the values you want to pass on
-        username: string, 
-        password: string
-    ): void
+  (
+    // First parameter, very important from submit-login, it will be identified in the parent component as onSubmitLogin
+    event: "submit-login",
+    // Second parameter, the values you want to pass on
+    username: string,
+    password: string,
+  ): void;
 }>();
 
-function submitForm(){
-    // Now we use the emit to pass on the parameters
-    emit('submit-login', username_value.value, password_value.value)
+function submitForm() {
+  // Now we use the emit to pass on the parameters
+  emit("submit-login", username_value.value, password_value.value);
 }
-
 </script>
 
 <template>
-    <div class="container">
-        <form @submit.prevent="submitForm" class="w-full max-w-lg">
-            <div class="flex flex-wrap -mx-3 mb-6">
-                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label for="username" v-bind="labelTailWindClass">Username</label>
-                    <input 
-                        type="text" 
-                        v-model="username_value" 
-                        name="username" id="username" 
-                        placeholder="Enter username"
-                        required="true"
-                        v-bind="inputTailWindClass">
-                </div>
-                <div class="w-full md:w-1/2 px-3">
-                    <label for="password" v-bind="labelTailWindClass">Password</label>
-                    <input 
-                        type="password" 
-                        v-model="password_value" 
-                        name="password" 
-                        id="password" 
-                        placeholder="Enter password"
-                        required="true"
-                        v-bind="inputTailWindClass">
-                </div>
-            </div>
+  <div class="container">
+    <form @submit.prevent="submitForm" class="w-full max-w-lg">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label for="username" v-bind="labelTailWindClass">Username</label>
+          <input
+            type="text"
+            v-model="username_value"
+            name="username"
+            id="username"
+            placeholder="Enter username"
+            required="true"
+            v-bind="inputTailWindClass"
+          />
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label for="password" v-bind="labelTailWindClass">Password</label>
+          <input
+            :type="showPassword ? 'text' : 'password'"
+            v-model="password_value"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            required="true"
+            v-bind="inputTailWindClass"
+          />
+        </div>
+      </div>
 
-            <button type="submit" class="bg-green-800 p-4 rounded-lg text-gray-100 text-lg font-semibold w-full">Login</button>
-        </form>
+      <button
+        type="submit"
+        class="bg-green-800 p-4 rounded-lg text-gray-100 text-lg font-semibold w-full"
+      >
+        Login
+      </button>
+    </form>
+    <div class="flex items-end justify-end px-3 gap-3 my-4">
+      <button @click="showPassword = !showPassword">
+        <EyeIcon v-if="!showPassword" class="w-5 h-5" />
+        <EyeSlashIcon v-else class="w-5 h-5" />
+      </button>
+
+      <p class="font-semibold">
+        {{ showPassword ? "Hide Password" : "Show Password" }}
+      </p>
     </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,62 +1,112 @@
 <script setup lang="ts">
-
-import { ref } from 'vue';
-import { labelTailWindClass, inputTailWindClass } from '@/styles/GlobalStyles';
-import { checkStrength } from '@/utils/Strength_Checker';
+import { ref } from "vue";
+import { labelTailWindClass, inputTailWindClass } from "@/styles/GlobalStyles";
+import { checkStrength } from "@/utils/Strength_Checker";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/vue/16/solid";
 
 const emit = defineEmits<{
-    (event:'submit-register', username: string, password: string, confirmPassword: string):void
+  (
+    event: "submit-register",
+    username: string,
+    password: string,
+    confirmPassword: string,
+  ): void;
 }>();
 
-const username= ref('');
-const password = ref('');
-const confirm_pass = ref('')
+const username = ref("");
+const password = ref("");
+const confirm_pass = ref("");
+const showPassword = ref(false);
 
-function submitForm(){
-    emit('submit-register', username.value, password.value, confirm_pass.value)
+function submitForm() {
+  emit("submit-register", username.value, password.value, confirm_pass.value);
 }
 
 // Password meter logic
-const passwordMeterLabel = ref('');
-const passwordMeterColor = ref('');
+const passwordMeterLabel = ref("");
+const passwordMeterColor = ref("");
 
 async function passwordMeter(password: string) {
-    let result = checkStrength(password);
+  let result = checkStrength(password);
 
-    passwordMeterLabel.value = result.label!;
-    passwordMeterColor.value = result.color!;
+  passwordMeterLabel.value = result.label!;
+  passwordMeterColor.value = result.color!;
 }
-
 </script>
 
 <template>
-    <div class="container">
-        <form @submit.prevent="submitForm">
-            <div class="mx-3 mb-6">
-                <div class="w-full px-3 mb-6 md:mb-0">
-                    <label for="username" v-bind="labelTailWindClass">Username</label>
-                    <input type="text" v-model="username" name="username" id="username" placeholder="Enter username" v-bind="inputTailWindClass" required="true">
-                </div>
-                <div class="w-full px-3">
-                    <label for="password" v-bind="labelTailWindClass">Password</label>
-                    <input type="password" v-on:input="passwordMeter(password)" v-model="password" name="password" id="password" placeholder="Enter password" v-bind="inputTailWindClass" required="true">               
-                </div>
-                <div class="my-4 mx-4 text-left font-semibold" v-bind:class="passwordMeterColor" id="password-div">
-                    <!-- Here is the password meter render -->
-                     {{ passwordMeterLabel }}
-                </div>
-                <div class="w-full px-3">
-                    <label for="password" v-bind="labelTailWindClass">Confirm Password</label>
-                    <input type="password" v-model="confirm_pass" name="confirm_pass" id="confirm_pass" placeholder="Confirm Password" v-bind="inputTailWindClass" required="true">
-                </div>
-            </div>
+  <div class="container">
+    <form @submit.prevent="submitForm">
+      <div class="mx-3 mb-6">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label for="username" v-bind="labelTailWindClass">Username</label>
+          <input
+            type="text"
+            v-model="username"
+            name="username"
+            id="username"
+            placeholder="Enter username"
+            v-bind="inputTailWindClass"
+            required="true"
+          />
+        </div>
+        <div class="w-full px-3">
+          <label for="password" v-bind="labelTailWindClass">Password</label>
 
+          <input
+            :type="showPassword ? 'text' : 'password'"
+            v-on:input="passwordMeter(password)"
+            v-model="password"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            v-bind="inputTailWindClass"
+            required="true"
+          />
+        </div>
+        <div
+          class="my-4 mx-4 text-left font-semibold"
+          v-bind:class="passwordMeterColor"
+          id="password-div"
+        >
+          <!-- Here is the password meter render -->
+          {{ passwordMeterLabel }}
+        </div>
+        <div class="w-full px-3">
+          <label for="password" v-bind="labelTailWindClass"
+            >Confirm Password</label
+          >
+          <input
+            :type="showPassword ? 'text' : 'password'"
+            v-model="confirm_pass"
+            name="confirm_pass"
+            id="confirm_pass"
+            placeholder="Confirm Password"
+            v-bind="inputTailWindClass"
+            required="true"
+          />
+        </div>
+      </div>
 
-            <button type="submit" class="bg-green-800 p-4 rounded-lg text-gray-100 text-lg font-semibold w-full">Register</button>
-        </form>
-    </div>
+      <div class="flex items-end justify-end px-3 gap-3 my-4">
+        <button @click="showPassword = !showPassword">
+          <EyeIcon v-if="!showPassword" class="w-5 h-5" />
+          <EyeSlashIcon v-else class="w-5 h-5" />
+        </button>
+
+        <p class="font-semibold">
+          {{ showPassword ? "Hide Password" : "Show Password" }}
+        </p>
+      </div>
+
+      <button
+        type="submit"
+        class="bg-green-800 p-4 rounded-lg text-gray-100 text-lg font-semibold w-full"
+      >
+        Register
+      </button>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
